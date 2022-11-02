a1ce75aa6c54a3588bc378d4bea76ba753ad93ca
Jesús Rueda-Becerril <jm.ruebe@gmail.com>
2022-11-01 17:41:13 -0700

    refactor: implementing floorFinder method

--- 2015/src/main/java/GetFloor.java
+++ 2015/src/main/java/GetFloor.java
@@ -5,19 +5,19 @@ public class GetFloor {
     public static void main(String[] args) {
         Scanner getInstructions = new Scanner(System.in);
         String instructions = getInstructions.next();
-        int count = 0;
+        System.out.println("Floor: " + floorFinder(instructions));
+    }
 
+    private static int floorFinder(String instructions) {
+        int count = 0;
         for (int i = 0; i < instructions.length(); i++) {
             if (instructions.charAt(i) == '(') {
                 count += 1;
             } else {
                 count -= 1;
             }
-
         }
-
-        System.out.println(String.format("Floor: %d", count));
-
+        return count;
     }
 
 }
